19906d0d8247df594f3853c9301a872e5e2a5208
Tales Montenegro <montenegro.tales@gmail.com>
2021-06-11 07:20:49 -0300

    finish smooth scroll when selectind a link

--- script.js
+++ script.js
@@ -15,3 +15,13 @@ function toggleMenu(event) {
 
 btnMobile.addEventListener('click', toggleMenu)
 btnMobile.addEventListener('touchstart', toggleMenu)
+
+document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault()
+
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth',
+    })
+  })
+})
